9167a9f94d39cb2486a30cae12dfd58315622639
Gesiel Rosa <gesielr@gmail.com>
2021-09-28 09:35:58 -0300

    remove unused files

--- projects/ngx-lazy-modal/src/lib/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './components/rhz-modal/rhz-modal.component';
-export * from './components/rhz-modal-content/rhz-modal-content.component';
-export * from './components/rhz-modal-footer/rhz-modal-footer.component';
-export * from './components/rhz-modal-header/rhz-modal-header.component';
-export * from './models/rhz-modal.model';
-export * from './rhz-modal.service';
-export * from './rhz-modal.module';
--- projects/ngx-lazy-modal/src/public-api.ts
+++ projects/ngx-lazy-modal/src/public-api.ts
@@ -1,3 +1,5 @@
 export * from './lib/modal.module';
 export * from './lib/components/modal/modal.component';
 export * from './lib/services/modal.service';
+export * from './lib/models/modal-ref.model';
+export * from './lib/models/modal.model';
